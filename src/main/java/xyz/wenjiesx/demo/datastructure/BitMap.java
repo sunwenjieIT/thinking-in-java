89d5430bc36e6f8e04199e2c324edd2839980ef8
wenji sun <sunwenjie@1dabang.cn>
2019-10-28 15:54:17 +0800

    bitmap数据结构 增加返回排序后的bitmap 数组的功能

--- src/main/java/xyz/wenjiesx/demo/datastructure/BitMap.java
+++ src/main/java/xyz/wenjiesx/demo/datastructure/BitMap.java
@@ -1,5 +1,8 @@
 package xyz.wenjiesx.demo.datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * bitmap的java实现 基于byte[]
  * 从0开始作为第一个数字
@@ -39,21 +42,44 @@ public class BitMap {
         bits[getIndex(num)] &= ~(1 << getPosition(num));
     }
 
+    public List<Integer> list() {
+
+        List<Integer> sortNums = new ArrayList<>();
+
+        for (int i = 0; i < bits.length; i++) {
+            byte bit = bits[i];
+            int tmp = 1;
+            for (int j = 0; j < 8; j++) {
+                if ((bit & tmp) != 0) {
+                    sortNums.add(i * 8 + j);
+                }
+                tmp = tmp << 1;
+            }
+        }
+        return sortNums;
+    }
+
     public static void main(String[] args) {
         BitMap bitMap = new BitMap(20000);
+        bitMap.setBit(20000);
         bitMap.setBit(500);
         bitMap.setBit(6666);
-        bitMap.setBit(20000);
+        bitMap.setBit(0);
         System.out.println(bitMap.contains(500));;
         System.out.println(bitMap.contains(20000));;
         System.out.println(bitMap.contains(6666));;
+        System.out.println(bitMap.list());
         bitMap.delBit(6);
         bitMap.delBit(6666);
         System.out.println(bitMap.contains(6666));
 
-        //测试数组越界的情况
+        List<Integer> list = bitMap.list();
+        System.out.println(list);
+
+//        测试数组越界的情况
         BitMap bit = new BitMap(7);
         bit.setBit(7);
         bit.setBit(8);
+
     }
 }
